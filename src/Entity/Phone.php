b0fb256552cdc27853a16319f3d0ee5d1423a380
Hédi Lannoo <hedilannoo@gmail.com>
2022-06-09 19:14:59 +0200

    add all requests and differents post constraints

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -2,30 +2,45 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PhoneRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PhoneRepository::class)]
+#[ApiResource(
+    formats: ['json'],
+    collectionOperations: ['get' => ['normalization_context' => ['groups' => 'read:collection_phone']]],
+    itemOperations: ['get' => ['normalization_context' => ['groups' => 'read:item_phone']]],
+    order: ["price" => "DESC"],
+
+)]
 class Phone
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read:collection_phone'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collection_phone','read:item_phone'])]
     private $name;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:item_phone'])]
     private $description;
 
     #[ORM\Column(type: 'float')]
+    #[Groups(['read:item_phone'])]
     private $price;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Groups(['read:item_phone'])]
     private $image;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:item_phone'])]
     private $brand;
 
     public function getId(): ?int
